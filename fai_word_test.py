0eb25a94d4630cd69b10872f954f2db47bfbd963
kripto63 <ignatov.fedor.evgenivich@gmail.com>
2025-03-26 21:19:19 +0400

    add golana ver3

--- fai_word_test.py
+++ fai_word_test.py
@@ -2,12 +2,14 @@ from golana.queue import QUEUE
 
 
 if __name__=="__main__":
-    data = 'with not and tot'
+    data1 = 'with not and tot'
+    data2 = 'bob cry with pay'
+    data3 = 'with ll pp dd'
     
     bd = QUEUE()
     print(bd.pointer)
     print(bd.get_data_registry())
-    bd.add_line(data)
+    bd.add_line(data1)
     print(bd.get_data_registry())
     bd.beginning()
     print(bd.get_data_registry()[0].link)
@@ -17,6 +19,19 @@ if __name__=="__main__":
     print(bd.get_data_registry()[0].link)
     print(bd.get_data_registry()[0].word)
 
+    bd.add_line(data2)
+    bd.add_line(data3)
+    bd.beginning()
+    print(bd.get_data_registry()[0].link)
+    print(bd.get_data_registry()[0].word)
+    print(bd.get_data_registry()[1].link)
+    print(bd.get_data_registry()[1].word)
+    print(bd.get_data_registry()[2].link)
+    print(bd.get_data_registry()[2].word)
+    
+    print(bd.get_data_registry())
+    print(bd.get_data_registry()[3].link)
+    print(bd.get_data_registry()[3].word)
     
         
     
